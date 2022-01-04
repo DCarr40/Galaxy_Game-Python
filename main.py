ffa9f7b0a5f7f8895b1b942d417251a68edd109f
DCarr40 <davidwcarruthers@yahoo.com>
2022-01-04 09:04:12 -0500

    fixed syntax issues and line 1 issue in kv file so now buttons display in app

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ from kivy.uix.widget import Widget
 class BoxLayoutExample(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-            b1 = Button()
+        b1 = Button(text="A")
+        b2 = Button(text="B")
+        self.add_widget(b1)
+        self.add_widget(b2)
+
+
 
 
 class MainWidget(Widget):
