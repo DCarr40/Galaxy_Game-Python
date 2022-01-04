caf691d12b305c2ef9d12b584d58c424980a16f2
DCarr40 <davidwcarruthers@yahoo.com>
2022-01-04 00:20:01 -0500

    created main widget class and imported Widget from kivy

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from kivy.app import App
+from kivy.uix.widget import Widget
 
+
+class MainWidget(Widget):
+    pass
 
 class TheLabApp(App):
     pass  # This pass is a syntax placeholder that is awesome so far!!
