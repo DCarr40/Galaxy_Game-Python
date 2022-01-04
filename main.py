55934453098e067177b4af11dcd81b8f0c480204
DCarr40 <davidwcarruthers@yahoo.com>
2022-01-04 08:55:03 -0500

    I importex Button and BoxLayout and began working on init boxlayout functions

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+
+
+class BoxLayoutExample(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+            b1 = Button()
 
 
 class MainWidget(Widget):
     pass
+
 
 class TheLabApp(App):
     pass  # This pass is a syntax placeholder that is awesome so far!!
